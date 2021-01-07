bf86a743f9698bf6b096e5208fd2243dbf9785b9
mandala21 <lucasresone@gmail.com>
2021-01-06 23:47:49 -0300

    FEATURE add list end point

--- src/utils/response.py
+++ src/utils/response.py
@@ -16,3 +16,10 @@ def single_response(data, status = 200):
         response=json.dumps(data),
         status=status
     )
+
+def collection_response(data, status = 200):
+    return Response(
+        mimetype="application/json",
+        response=json.dumps({'result': data}),
+        status=status
+    )
--- src/views/InventoryView.py
+++ src/views/InventoryView.py
@@ -1,7 +1,7 @@
 from flask import request, json, Response, Blueprint
 from src.models.Inventory import Inventory
 from src.serializers.InventorySerializer import inventory_serializer
-from src.utils.response import bad_request, single_response
+from src.utils.response import bad_request, single_response, collection_response
 from marshmallow.exceptions import ValidationError
 
 inventory_blueprint = Blueprint('inventory', __name__)
@@ -23,3 +23,9 @@ def create():
     #serialize response
     res_instance = inventory_serializer.dump(instance)
     return single_response(res_instance, 201)
+
+@inventory_blueprint.route('/', methods=['GET'])
+def index():
+    db_collections = Inventory.query.all()
+    ser_collection = inventory_serializer.dump(db_collections, many=True)
+    return collection_response(ser_collection)
